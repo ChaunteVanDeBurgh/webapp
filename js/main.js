551bba3dedc0c90e25f5d85c7bc779c7719184ad
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-11-16 15:45:25 -0600

    afix profile section

--- js/main.js
+++ js/main.js
@@ -1,35 +1,48 @@
 $(document).ready(function() {
-	var $charCount, maxCharCount;
+    var $charCount, maxCharCount;
 
-	$charCount = $("#tweet-modal .char-count")
-	maxCharCount = parseInt($charCount.data('max'), 10);
-	$("#tweet-modal textarea").on('keyup', function(e) {
-		var tweetLength = $(e.currentTarget).val().length;
+    $charCount = $('#tweet-modal .char-count')
+    maxCharCount = parseInt($charCount.data('max'), 10);
 
-		$charCount.html(maxCharCount - tweetLength);
+    $('#tweet-modal textarea').on('keyup', function(e) {
+        var tweetLength = $(e.currentTarget).val().length;
+
+        $charCount.html(maxCharCount - tweetLength);
 	});
+    $('[data-toggle="tooltip"]').tooltip();
 
-	$('[data-toggle="tooltip"]').tooltip();
-	$('[data-toggle="popover"]').popover();
+    var popoverContentTemplate = '' +
+        '<img src="./imgs/breed.jpg" class="img-rounded">' +
+        '<div class="info">' +
+            '<strong>Dog Breeds</strong>' +
+            '<a href="#" class="btn btn-default">' +
+                '<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>' +
+                'Follow' +
+            '</a>' +
+        '</div>';
 
-	// Pop Over 
+    $('[data-toggle="popover"]').popover({
+        placement: 'bottom',
+        html: true,
+        content: function() {
+            return popoverContentTemplate;
+        }
+    });
 
-	var popoverContentTemplate = '' +
-		'<img src="imgs/breed.jpg" class="img-rounded">' +
-		'<div class="info">' +
-			'<strong>Dog Breeds</strong>' +
-			'<a href="#" class="btn btn-default">' +
-			'<span class="glyphicon glyphicon-plus" aria-hidden="true"</span>'
+    $('[data-toggle="popover"]').on('show.bs.popover', function() {
+        var $icon = $(this).find('span.glyphicon');
 
-			'Follow' +
-			'</a>' +
-			'</div>';
+        $icon.removeClass('glyphicon-plus').addClass('glyphicon-ok');
+        // $(this).append('ing');
+    });
 
-	$('[data-toggle="popover"]').popover({
-		placement: 'bottom',
-		html: true,
-		content: function() {
-			return popoverContentTemplate;
-		}
-	});
+    // $('#profile').affix();
+
+    $('#profile').on('affix.bs.affix', function () {
+    	$(this).width($(this).width() - 1);
+    	$('#main').addClass('col-md-offset-3');	
+    }).on('affix-top.bs.affix',function() {
+    	$(this).css('width', '');
+    	$('#main').removeClass('col-md-offset-3');
+    });
 });
